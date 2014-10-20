f33af58419b670c7be5db7254488bd18ead05e30
Bryant <slimer07@vt.edu>
2014-10-19 21:45:52 -0700

    Adding new data set and review script update

--- MATLAB/AccelDataReview.m
+++ MATLAB/AccelDataReview.m
@@ -1,6 +1,12 @@
 
+%% Data Set One
+accelData1 = csvread('/Users/bryant/Documents/GitHub Repos/ArduinoBikeShirt/AccelData/AccelData.csv');
+time = 0:0.01:(length(accelData1(:,1))-1)*0.01;
+filtAccel = filter(Hd,accelData1(:,3));
+plot(time',[accelData1(:,3) filtAccel])
 
-accelData = csvread('/Users/bryant/Documents/GitHub Repos/ArduinoBikeShirt/AccelData/AccelData.csv')
-time = 0:0.01:(length(accelData(:,1))-1)*0.01;
-
-plot(time',accelData)
+%% Data Set 2
+accelData2 = csvread('/Users/bryant/Documents/GitHub Repos/ArduinoBikeShirt/AccelData/AccelData2.csv');
+time = 0:0.01:(length(accelData2(:,1))-1)*0.01;
+filtAccel = filter(Hd,accelData2(:,3));
+plot(time',[accelData2(:,3) filtAccel])
