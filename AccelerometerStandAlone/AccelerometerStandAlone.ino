7486320d87a5023861207b5253cd593d5ad35cf8
Bryant <slimer07@vt.edu>
2014-10-26 21:17:04 -0700

    Updated Accel Function so that is doesn't live in the main loop

--- AccelerometerStandAlone/AccelerometerStandAlone.ino
+++ AccelerometerStandAlone/AccelerometerStandAlone.ino
@@ -4,30 +4,50 @@
  
  This example code is in the public domain.
  */
+ //**********************************************************
+//  Global Vars
+//**********************************************************
 int minVol = 267;
 int maxVol = 400;
+int accelData[3];
+//**********************************************************
+//  SETUP LOOP
+//**********************************************************
+
 void setup() {
-  Serial.begin(9600);
+Serial.begin(9600);
   
 
 }
-
+//**********************************************************
+//    MAIN LOOP
+//**********************************************************
 void loop() {
-  int sensorValuex = analogRead(A3);
-  int sensorValuey = analogRead(A2);
-  int sensorValuez = analogRead(A1);
-  
-  int gReadX = map(sensorValuex,minVol,maxVol,-1000,1000);
-  int gReadY = map(sensorValuey,minVol,maxVol,-1000,1000);
-  int gReadZ = map(sensorValuez,minVol,maxVol,-1000,1000);
-  
-  Serial.print(gReadX);
+
+  readAccel();
+  Serial.print(accelData[0]);
    Serial.print(",");
-  Serial.print(gReadY);
+  Serial.print(accelData[1]);
    Serial.print(",");
-  Serial.println(gReadZ);
+  Serial.println(accelData[2]);
   delay(10);
     
   
   
+}
+
+//**********************************************************
+//    Read Accelerometer Data from Pins A1A2A3
+//**********************************************************
+void readAccel(){
+  
+  int sensorValuex = analogRead(A3);
+  int sensorValuey = analogRead(A2);
+  int sensorValuez = analogRead(A1);
+  
+  //Place in Arrays to place up
+  accelData[0] = map(sensorValuex,minVol,maxVol,-1000,1000);
+  accelData[1] = map(sensorValuey,minVol,maxVol,-1000,1000);
+  accelData[2] = map(sensorValuez,minVol,maxVol,-1000,1000);
+
 }
